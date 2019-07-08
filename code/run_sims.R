c9860526f10f6acd45f880dfb7d1e85fc9f2901a
David Gerard <gerard.1787@gmail.com>
2019-07-08 10:25:52 -0400

    - Returns median proportion of variance explained in the factor analysis simulations. - Adjusts correlation ranges to just 0 and 0.5. - Based on median PVE results, change loading sd to 0.4, 0.7, and 1. This will probably result in more informative conclusions regarding changing signal strength. - Decrease tolerance for calculating angle. This should help PEER. - Fix bug in calculating MSE. I was doing mse with fl$factors instead of thout$designmat.

--- code/run_sims.R
+++ code/run_sims.R
@@ -7,7 +7,7 @@
 ##       interactive mode, you need to modify the below code so that nc
 ##       is not specified by commandArgs().
 #################
-library(doSNOW)
+suppressPackageStartupMessages(library(doSNOW))
 
 # Number of threads to use for multithreaded computing. This must be
 # specified in the command-line shell; e.g., to use 8 threads, run
@@ -45,6 +45,10 @@ one_sim_iter <- function(obj, bigmat) {
                pz    = obj$pz,
                lsd   = obj$lsd)
 
+  ## Median proportion of variance explained ----------------------------------
+  flprod <- tcrossprod(fl$loadings, fl$factors)
+  mpve <- median(apply(flprod, 1, stats::var) / apply(lmat, 1, stats::var))
+
   ## Add factors and loadings to mat ------------------------------------------
   thout <- seqgendiff::thin_diff(mat         = mat,
                                  design_perm = fl$factors,
@@ -70,7 +74,7 @@ one_sim_iter <- function(obj, bigmat) {
   ## Angle between X3 and columnspace of estimate -----------------------------
   anglevec <- sapply(faafter, FUN = function(obj) {
     svout <- svd(obj$factors)
-    smat <- svout$u[, svout$d > 10^-6]
+    smat <- svout$u[, svout$d > 10^-12, drop = FALSE] ## make sure projection matrix is not computationally singular.
     X3hat <- smat %*% solve(crossprod(smat)) %*% crossprod(smat, thout$designmat)
     X3 <- thout$designmat
     X3 <- X3 / sqrt(sum(X3 ^ 2))
@@ -79,16 +83,16 @@ one_sim_iter <- function(obj, bigmat) {
   })
 
   ## MSE between scaled added factor and scaled estimated factors -------------
-
   msevec <- sapply(faafter, FUN = function(obj) {
-    min(colMeans((scale(obj$factors, center = FALSE) - scale(fl$factors, center = FALSE)[, 1]) ^ 2))
+    min(colMeans((scale(obj$factors, center = FALSE) - scale(thout$designmat, center = FALSE)[, 1]) ^ 2))
   })
 
   ## Change names and combine for output --------------------------------------
   names(cordiff)  <- paste0("cordiff_", names(cordiff))
   names(anglevec) <- paste0("angle_", names(anglevec))
   names(msevec)   <- paste0("mse_", names(msevec))
-  retvec <- c(cordiff, anglevec, msevec, nsv = nsv + 1, unlist(obj))
+  retvec <- c(cordiff, anglevec, msevec, nsv = nsv + 1, unlist(obj),
+              mpve = mpve)
 
   return(retvec)
 }
@@ -103,9 +107,9 @@ bigmat    <- assay(musc)
 ## Set simulation parameters --------------------------------------------------
 ngene     <- 1000             ## Number of genes
 nsamp_vec <- c(6, 10, 20)     ## Number of samples
-cor_list  <- list(c(0, 0), c(0.9, 0)) ## Target correlation between new factor and two old factors
+cor_list  <- list(c(0, 0), c(0.5, 0)) ## Target correlation between new factor and two old factors
 prop_zero <- c(0, 0.5, 0.9)   ## Proportion of loadings that are 0.
-load_sd   <- c(1, 2, 3)       ## Standard deviation of loadings.
+load_sd   <- c(0.4, 0.7, 1)   ## Standard deviation of loadings.
 itermax   <- 200              ## Number of iterations per condition
 seedindex <- seq_len(itermax)
 
